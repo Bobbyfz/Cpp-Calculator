b05fe9dcb9f52ed7dc4bf69ae84ec05c507ede6c
Robert Feliz <Robert_Feliz@student.uml.edu>
2020-01-01 17:29:38 -0500

    Removed C libraries and instead replaced with C++
    The buffer clearing functions have been moved from C to the C++ methods

--- main.cpp
+++ main.cpp
@@ -1,52 +1,52 @@
 #include <iostream>
-#include <stdio.h>
 #include <math.h>
-
-void clearKeyboardBuffer(void);
 
 int main()
 {
-	char op;
-	double firstOperand;
-	double secondOperand;
+    char op;
+    double firstOperand;
+    double secondOperand;
 
-	do
-	{
-		std::cout << "Enter an operator (+,-,/,*,^)\n";
-		std::cin >> op;
-		clearKeyboardBuffer();
-	} while (op != '+' && op != '-' && op != '/' && op != '*'&&op != '^');
-	
-	std::cout << "Enter 2 operands (any int)\n";
-	std::cin >> firstOperand >> secondOperand;
-	
-	switch (op)
-	{
-	case '+': 
-		std::cout << firstOperand << " added to " << secondOperand << " is equal to: " << firstOperand + secondOperand;
-		break;
-	case '-':
-		std::cout << firstOperand << " subtracted from " << secondOperand << " is equal to: " << firstOperand - secondOperand;
-		break;
-	case '/':
-		std::cout << firstOperand << " divided by " << secondOperand << " is equal to: " << firstOperand / secondOperand;
-		break;
-	case '*':
-		std::cout << firstOperand << " multiplied by " << secondOperand << " is equal to: " << firstOperand * secondOperand;
-		break;
-	case '^':
-		std::cout << firstOperand << " raised to " << secondOperand << " is equal to: " << pow(firstOperand,secondOperand);
-	}
+    do
+    {
+        std::cout << "Enter an operator (+,-,/,*,^)\n";
+        std::cin >> op;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    } while (op != '+' && op != '-' && op != '/' && op != '*' && op != '^');
+
+    std::cout << "Enter 2 operands (any int)\n";
+    std::cin >> firstOperand >> secondOperand;
+
+    while (!std::cin)
+    {
+        std::cout << "Thats not an int, please enter an integer: \n";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> firstOperand >> secondOperand;
+    }
+
+    switch (op)
+    {
+    case '+':
+        std::cout << firstOperand << " added to " << secondOperand << " is equal to: " << firstOperand + secondOperand<<"\n";
+        break;
+    case '-':
+        std::cout << firstOperand << " subtracted from " << secondOperand << " is equal to: " << firstOperand - secondOperand << "\n";
+        break;
+    case '/':
+        std::cout << firstOperand << " divided by " << secondOperand << " is equal to: " << firstOperand / secondOperand << "\n";
+        break;
+    case '*':
+        std::cout << firstOperand << " multiplied by " << secondOperand << " is equal to: " << firstOperand * secondOperand << "\n";
+        break;
+    case '^':
+        std::cout << firstOperand << " raised to " << secondOperand << " is equal to: " << pow(firstOperand, secondOperand) << "\n";
+        break;
+    default:
+        std::cout << "Error\n";
+        break;
+
+    }
 }
 
-void clearKeyboardBuffer(void)
-{
-	char ch;
-
-	scanf_s("%c", &ch,1);
-
-	while (ch != '\n' && ch != '\0')
-	{
-		scanf_s("%c", &ch, 1);
-	}
-}
