2d33ca21142fa2b21175ec0d63c20f6e4d321391
Robert Feliz <Robert_Feliz@student.uml.edu>
2020-01-05 14:01:09 -0500

    Final commit Finished the calculator and added the option for repetition Cleaned up main and added 2 functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <math.h>
 
+void operate(char op, double firstOperand, double secondOperand);
+int keepGoing(void);
+
 int main()
 {
     char op;
@@ -9,27 +12,39 @@ int main()
 
     do
     {
-        std::cout << "Enter an operator (+,-,/,*,^)\n";
-        std::cin >> op;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    } while (op != '+' && op != '-' && op != '/' && op != '*' && op != '^');
+        do
+        {
+            std::cout << "Enter an operator (+,-,/,*,^)\n";
+            std::cin >> op;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    std::cout << "Enter 2 operands (any int)\n";
-    std::cin >> firstOperand >> secondOperand;
+        } while (op != '+' && op != '-' && op != '/' && op != '*' && op != '^');
 
-    while (!std::cin)
-    {
-        std::cout << "Thats not an int, please enter an integer: \n";
-        std::cin.clear();
-        std::cin.ignore();
+        std::cout << "Enter 2 operands (any int)\n";
         std::cin >> firstOperand >> secondOperand;
-    }
+
+        while (!std::cin)
+        {
+            std::cout << "Thats not an int, please enter an integer (both numbers have been cleared): \n";
+            std::cin.clear();
+            std::cin.ignore();
+            std::cin >> firstOperand >> secondOperand;
+        }
+
+        operate(op, firstOperand, secondOperand);
+        keepGoing();
+
+    } while (keepGoing != 0);
+}
+
+void operate(char op, double firstOperand, double secondOperand)
+{
 
     switch (op)
     {
     case '+':
-        std::cout << firstOperand << " added to " << secondOperand << " is equal to: " << firstOperand + secondOperand<<"\n";
+        std::cout << firstOperand << " added to " << secondOperand << " is equal to: " << firstOperand + secondOperand << "\n";
         break;
     case '-':
         std::cout << firstOperand << " subtracted from " << secondOperand << " is equal to: " << firstOperand - secondOperand << "\n";
@@ -50,3 +65,25 @@ int main()
     }
 }
 
+int keepGoing(void)
+{
+    char choice;
+
+    do
+    {
+
+    std::cout << "Would you like to calculate again? (y/n)\n";
+    std::cin >> choice;
+
+    } while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N');
+
+    if (choice == 'y' || choice == 'Y')
+    {
+        return 1;
+    }
+    else if (choice == 'n' || choice == 'N')
+    {
+        exit(0);
+    }
+    
+}
